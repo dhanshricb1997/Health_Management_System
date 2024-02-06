0d5c1dfc1bb36bd48f9148ae835b725e90eef785
Dhanshri Borole <dhanshricb1997@gmail.com>
2024-02-06 12:46:29 +0530

    Add project files.

---
+++ Health_Management_System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Health_Management_System.MvcApplication" Language="C#" %>
---
+++ Health_Management_System/Repository/DAC.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Health_Management_System.Repository
+{
+    public class DAC
+    {
+    }
+}
