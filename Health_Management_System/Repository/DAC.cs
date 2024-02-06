8b6609a2dd080ac2cd938696a7881483a1a8813a
Dhanshri Borole <dhanshricb1997@gmail.com>
2024-02-06 17:05:57 +0530

    Add a post method

--- Health_Management_System/Repository/DAC.cs
+++ Health_Management_System/Repository/DAC.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,26 @@ namespace Health_Management_System.Repository
 {
     public class DAC
     {
+        string con = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        public void addData()
+        {
+            
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO People (Name, Age) VALUES (@Name, @Age)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    //command.Parameters.AddWithValue("@Name", person.Name);
+                    //command.Parameters.AddWithValue("@Age", person.Age);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        
+        
     }
+
 }
