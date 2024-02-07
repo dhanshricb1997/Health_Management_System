b203d358676cf7e1d33513e2fa8fc75623849837
Dhanshri Borole <dhanshricb1997@gmail.com>
2024-02-07 12:11:22 +0530

    Add a dashboard page

---
+++ Health_Management_System/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Health_Management_System.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
