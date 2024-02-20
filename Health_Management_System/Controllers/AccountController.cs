600ba0eed649581aa34fabb6d3fa3cc6971db7b2
Laxmikant Birajdar <birajdarlax@gmail.com>
2024-02-20 14:24:07 +0530

    registration form

--- Health_Management_System/Controllers/AccountController.cs
+++ Health_Management_System/Controllers/AccountController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Health_Management_System.Models;
+using System.Data;
+using Health_Management_System.Repository;
 
 namespace Health_Management_System.Controllers
 {
@@ -17,9 +20,50 @@ namespace Health_Management_System.Controllers
         {
             return View();
         }
+        [HttpGet]
         public ActionResult registartionForm()
         {
+           
+            
+                Registation obj1 = new Registation();
+                DAC abc = new DAC();
+                DataTable dt = new DataTable();
+
+                dt = abc.dropDown();
+                List<SelectListItem> roleoptions = dt.AsEnumerable().Select(row => new SelectListItem
+                {
+                    Value = row["DocId"].ToString(),
+                    Text = row["Role"].ToString()
+                }).ToList();
+
+                var model = new Registation
+                {
+                   role = roleoptions
+                };
+                //return View(model.role);
+            
+            return Json(model.role, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult GetDropdownOptions()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Registation model)
+        {
+            if(ModelState.IsValid)
+            {
+            return View("registartionForm", model);
+
+            }
+            else
+            {
+                return View();
+            }
+            
         }
     }
 }
--- Health_Management_System/Repository/DAC.cs
+++ Health_Management_System/Repository/DAC.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
 
 namespace Health_Management_System.Repository
 {
     public class DAC
     {
+        public DataTable dropDown()
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("Role");
+            dt.Columns.Add("DocId");
+
+            dt.Rows.Add("Doctor", "1");
+            dt.Rows.Add("Nurse", "2");
+            dt.Rows.Add("Patient", "3");
+            dt.Rows.Add("Staff", "4");
+            
+            return dt;
+        }
     }
 }
